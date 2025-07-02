32a895e3cbe68694af1150684ac185caec317106
whilrod <whilrod@gmail.com>
2025-07-01 23:54:06 -0500

    Inicializar variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo de dato + nombre (+ valoropcional)
+            int numero;
+            numero = 12;
+            int otroNumero = 23; //inicializada inline
+            Console.WriteLine(numero + "\n"+ otroNumero);
+        }
+    }
+}
