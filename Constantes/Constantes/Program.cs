3f8223df8da466c351cfbe0bd0a65513f693b46d
whilrod <whilrod@gmail.com>
2025-07-03 13:09:50 -0500

    Constantes

---
+++ Constantes/Constantes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int NUMERO = 23; //tipo y mayusculas 
+            Console.WriteLine(NUMERO);
+        }
+    }
+}
