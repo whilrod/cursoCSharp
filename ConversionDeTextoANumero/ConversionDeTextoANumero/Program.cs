2015817e5c35779e64d52126dee288918259cf67
whilrod <whilrod@gmail.com>
2025-07-03 12:38:57 -0500

    conversion de texto a numero

---
+++ ConversionDeTextoANumero/ConversionDeTextoANumero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionDeTextoANumero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // .Parse()
+            Console.WriteLine("Ingrese el primer valor");
+            int valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo valor");
+            int valor2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado concatenado es: " + (valor1 * valor2));
+            Console.WriteLine($"El resultado interpolado es: {valor1 * valor2}");
+        }
+    }
+}
