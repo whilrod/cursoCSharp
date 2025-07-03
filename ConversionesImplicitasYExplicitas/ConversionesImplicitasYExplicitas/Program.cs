d1b9fe96a9ee65e0306d7f4e972e58bf28ba3cb2
whilrod <whilrod@gmail.com>
2025-07-03 12:22:15 -0500

    Conversion Implicita y Explicita

---
+++ ConversionesImplicitasYExplicitas/ConversionesImplicitasYExplicitas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversionesImplicitasYExplicitas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conversion Implicita
+            double numero = 10;
+            Console.WriteLine(numero);
+
+            // Conversion Explicita
+            int valor = (int)4.5;
+            Console.WriteLine(valor);
+            Console.WriteLine(valor);
+        }
+    }
+}
