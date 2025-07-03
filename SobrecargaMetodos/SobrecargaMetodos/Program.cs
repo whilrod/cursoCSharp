19f95acaaace1093e791967e414c225e8f25291f
whilrod <whilrod@gmail.com>
2025-07-03 15:48:55 -0500

    Sobrecarga de metodos

---
+++ SobrecargaMetodos/SobrecargaMetodos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace SobrecargaMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(suma(2,3));
+            Console.WriteLine(suma(2, 3, 5.9));
+        }
+        //mismo nombre pero diferente tipo de parametros o diferente cantidad de parametros
+
+        static int suma(int numero1, int numero2) => numero1 + numero2;
+
+        static int suma(int numero1, int numero2, int numero3) => numero1 + numero2 + numero3;
+
+        static double suma(int numero1, int numero2, double numero3 = 0 ) => numero1+ numero3;
+    }
+}
