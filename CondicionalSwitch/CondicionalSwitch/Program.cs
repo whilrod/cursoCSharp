ed7b3d16e70fd43d90b851d6eee7cdc4f74afce0
whilrod <whilrod@gmail.com>
2025-07-04 00:49:37 -0500

    condicional switch

---
+++ CondicionalSwitch/CondicionalSwitch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionalSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elegir comida: (ceviche, tacos o paella): ");
+            string comida = Console.ReadLine();
+            switch (comida)
+            {
+                case "ceviche":
+                    Console.WriteLine("Comida Peruana");
+                    break;
+                case "tacos":
+                    Console.WriteLine("Comida Mexicana");
+                    break;
+                case "paella":
+                    Console.WriteLine("Comida Española");
+                    break;
+                default:
+                    Console.WriteLine("Comida no especificada");
+                    break;
+            }
+        }
+    }
+}
