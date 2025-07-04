4cea40c52350f32278dedbe574b6bba7e1e3c2bd
whilrod <whilrod@gmail.com>
2025-07-04 10:24:25 -0500

    Bucles while y do while

---
+++ BucleWhile/BucleWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BucleWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciar bucle?");
+            string respuesta = Console.ReadLine();
+
+            while (respuesta!= "no")
+            {
+                Console.WriteLine("Ejecutando el bucle...");
+                Console.WriteLine("Desea continuar?");
+                respuesta = Console.ReadLine();
+            }
+
+            Console.WriteLine("Saliendo del bucle");
+
+            do{
+                Console.WriteLine("Ejecutando el bucle...");
+                Console.WriteLine("Desea continuar?");
+                respuesta = Console.ReadLine();
+            } while (respuesta != "no");
+
+        }
+    }
+}
